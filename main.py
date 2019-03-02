2cef2adb8697b0dab2c3154b0c32c6de476c240f
simonseyock <simonseyock@googlemail.com>
2019-03-02 22:06:35 +0100

    game list

--- main.py
+++ main.py
@@ -37,26 +37,49 @@ def login():
         'username': uname,
         'password': pw
     })
-    return response.json()['access_token']
+    return uname, response.json()['access_token']
 
 def debug_login():
     uname = 'PinkPanther'
-    pw = 'test1'
-    response = post('/oauth2/token/', {
+    access_token = post('/oauth2/token/', {
         'client_id': secrets['client_id'],
         'client_secret': secrets['client_secret'],
         'grant_type': 'password',
         'username': uname,
-        'password': pw
-    })
-    return response.json()['access_token']
+        'password': 'test1'
+    }).json()['access_token']
+    return get('/api/v1/me/', access_token=access_token).json(), access_token
 
+def list_open_games(user, access_token):
+    games = get('/api/v1/me/games/', access_token=access_token).json()['results']
+    game_details = []
+    for (i, game) in enumerate(games, 1):
+        game_detail = get(game['related']['detail']).json()
 
-def list_open_games(access_token):
-    response = get('/api/v1/me/games/', access_token=access_token)
-    print(response.json())
+        if game_detail['players']['white']['id'] == user['id']:
+            opponent = game_detail['players']['black']
+        else:
+            opponent = game_detail['players']['white']
 
+        if game_detail['gamedata']['clock']['current_player'] == user['id']:
+            active_player = user
+        else:
+            active_player = opponent
+        print('{key}) {player1} (B) vs {player2} (W) | {active_player} to move.'.format(
+            key = i,
+            active_player = active_player['username'],
+            player1 = game_detail['players']['black']['username'],
+            player2 = game_detail['players']['white']['username']))
+
+        game_details.append(game_detail)
+    return game_details
+
+def choose_game(user, access_token):
+    games = list_open_games(user, access_token)
+    key = int(input("Choose game (number): "))
+    return games[key - 1]
 
 if __name__ == '__main__':
-    access_token = debug_login()
-    list_open_games(access_token)
+    user, access_token = debug_login()
+    game = choose_game(user, access_token)
+    print(game)
