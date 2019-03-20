013f408346e8b525508d78897ca63abd340cb20a
simonseyock <simonseyock@googlemail.com>
2019-03-20 18:10:05 +0100

    initialized setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ogs_cli',
+      version='0.1',
+      description='Command line interface for online-go.com',
+      url='http://github.com/simonseyock/ogs_cli',
+      author='Milan Kroemer, Simon Seyock',
+      author_email='milan.kroemer@pm.me, simonseyock@gmail.com',
+      license='MIT',
+      packages=['ogs_cli'],
+      zip_safe=False)
