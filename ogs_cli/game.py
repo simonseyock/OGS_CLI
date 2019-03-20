61de0303f2bf78cc202d3da4be7f21f53ab7ccb0
simonseyock <simonseyock@googlemail.com>
2019-03-20 21:52:57 +0100

    use square

--- ogs_cli/game.py
+++ ogs_cli/game.py
@@ -2,6 +2,7 @@ from .request_handler import *
 from sgfmill import sgf
 from sgfmill import sgf_moves
 from colorama import Fore, Back, Style
+from wcwidth import wcswidth
 
 def print_sgf(game, access_token):
     sgf_string = get('/api/v1/games/' + str(game['id']) + '/sgf/', access_token=access_token, header=1).text
@@ -11,14 +12,17 @@ def print_sgf(game, access_token):
     for move in moves:
         board[move[1][0]][move[1][1]] = move[0]
 
+    print(wcswidth(' '))
+    print(wcswidth(u'\u26ab'))
+
     for row in board:
         for field in row:
             if field is None:
-                print(Back.YELLOW + '  ', end='')
+                print(Fore.YELLOW + u'\u25a0', end='')
             elif field == 'b':
-                print(Back.YELLOW + Fore.BLACK + u'\u25cf', end='')
+                print(Fore.BLACK+ u'\u25a0', end='')
             elif field == 'w':
-                print(Back.YELLOW + Fore.WHITE + u'\u25cf', end='')
-        print(Back.WHITE + '')
+                print(Fore.WHITE + u'\u25a0', end='')
+        print('')
 
     print(Style.RESET_ALL)
